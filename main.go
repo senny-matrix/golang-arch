3a846693f3ffaad303713f080f8b4eb5ca10a4a4
Rogers Aaron <arumeru@gmail.com>
2021-03-04 20:58:34 +0300

    Basic Authentication (Base64 user:pass example)

--- main.go
+++ main.go
@@ -1,48 +1,10 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/base64"
 	"fmt"
-	"log"
-	"net/http"
 )
 
-type person struct {
-	First string `json:"first"`
-}
-
 func main() {
-
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", bar)
-
-	if err := http.ListenAndServe(":8088", nil); err != nil {
-		log.Fatalln("Something went wrong starting web server:", err)
-	}
-}
-
-func foo(w http.ResponseWriter, r *http.Request) {
-	p1 := person{
-		First: "Senny",
-	}
-
-	p2 := person{
-		First: "James",
-	}
-
-	people := []person{p1, p2}
-
-	err := json.NewEncoder(w).Encode(people)
-	if err != nil {
-		log.Println("Bad data to encode", err)
-	}
-}
-
-func bar(w http.ResponseWriter, r *http.Request)  {
-	people := []person{}
-	err := json.NewDecoder(r.Body).Decode(&people)
-	if err != nil {
-		log.Println("bad data to decode:", err)
-	}
-	fmt.Println(people)
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
 }
